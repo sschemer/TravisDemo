e04d637653547e24b697a120f2e115dde2d0b6a0
loopeer-wanggang <wanggang@loopeer.com>
2016-08-08 16:36:45 +0800

    add test code

---
+++ app/src/main/java/me/bytecho/travisdemo/util/MathUtils.java
@@ -0,0 +1,20 @@
+package me.bytecho.travisdemo.util;
+
+public class MathUtils {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int minus(int a, int b) {
+        return a - b;
+    }
+
+    public static int mult(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(int a, int b) {
+        return a / b;
+    }
+}
---
+++ app/src/test/java/me/bytecho/travisdemo/util/MathUtilsTest.java
@@ -0,0 +1,28 @@
+package me.bytecho.travisdemo.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MathUtilsTest {
+
+    @Test
+    public void testAdd() throws Exception {
+        assertEquals(8, MathUtils.add(3, 5));
+    }
+
+    @Test
+    public void testMinus() throws Exception {
+        assertEquals(1, MathUtils.minus(4, 3));
+    }
+
+    @Test
+    public void testMult() throws Exception {
+        assertEquals(12d, MathUtils.mult(3, 4), 0);
+    }
+
+    @Test
+    public void testDivide() throws Exception {
+        assertEquals(3d, MathUtils.divide(12, 4), 0);
+    }
+}
